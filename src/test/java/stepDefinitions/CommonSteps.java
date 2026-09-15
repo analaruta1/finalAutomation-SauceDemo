6ecb11f4f9fe0fa9e6de0750b517fd35e0d8f160
analaruta1 <analaruta1@upb.edu>
2026-09-15 00:29:01 -0400

    Se añadieron los steps para el menu y modificaciones

--- src/test/java/stepDefinitions/CommonSteps.java
+++ src/test/java/stepDefinitions/CommonSteps.java
@@ -1,13 +1,27 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import pages.HeaderPage;
 import utils.DriverManager;
 
 public class CommonSteps {
+    HeaderPage headerPage = new HeaderPage(DriverManager.getDriver().driver);
 
     @Given("I am in sauce demo web page")
     public void goToSauceDemoPage(){
-        DriverManager.getDriver().driver.get("https://www.saucedemo.com");
-        DriverManager.getDriver().driver.manage().window().maximize();
+        DriverManager.getDriver().driver.get(DriverManager.BASE_URL);
+    }
+
+    @When("I navigate directly to {string}")
+    public void navigateDirectlyTo(String path){
+        DriverManager.getDriver().driver.get(DriverManager.BASE_URL + path);
+    }
+
+    @Then("The page title should be {string}")
+    public void verifyPageTitle(String expectedTitle){
+        Assertions.assertEquals(expectedTitle, headerPage.getPageTitle());
     }
 }
